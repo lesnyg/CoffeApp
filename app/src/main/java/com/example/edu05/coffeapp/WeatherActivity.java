bc1d34c09c0cc5ceff1d34c06e232c726855e26f
lesnyg <44351753+lesnyg@users.noreply.github.com>
2018-12-19 14:00:38 +0900

    memoapp,save

--- app/src/main/java/com/example/edu05/coffeapp/WeatherActivity.java
+++ app/src/main/java/com/example/edu05/coffeapp/WeatherActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +15,13 @@ import java.util.List;
 import adapters.WeatherAdapter;
 import models.Weather;
 
-public class WeatherActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class WeatherActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView mListView;
     private WeatherAdapter madapter;
+    private List<Weather> mWeatherList;
+    private GridView mGridView;
+    private Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,34 +29,44 @@ public class WeatherActivity extends AppCompatActivity implements AdapterView.On
         setContentView(R.layout.activity_weather);
 
         mListView = findViewById(R.id.list_view);
+        mGridView = findViewById(R.id.grid_view);
+        mSpinner = findViewById(R.id.spinner);
 
         //날씨 데이터
-        List<Weather> weatherList = new ArrayList<>();
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"충청","27도"));
-        weatherList.add(new Weather(R.drawable.ic_rain,"강원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_rain,"부산","20도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"서울","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_snow,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_snow,"부산","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_windy,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_windy,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_rain,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_stormy,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_rain,"수원","27도"));
-        weatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList = new ArrayList<>();
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"충청","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_rain,"강원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_rain,"부산","20도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"서울","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_snow,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_snow,"부산","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_windy,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_windy,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_rain,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_stormy,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_rain,"수원","27도"));
+        mWeatherList.add(new Weather(R.drawable.ic_sun,"수원","27도"));
 
-        madapter = new WeatherAdapter(this,weatherList);
+        //어댑터
+        madapter = new WeatherAdapter(this, mWeatherList);
+        //어댑터를 뷰에 설정
         mListView.setAdapter(madapter);
+        mGridView.setAdapter(madapter);
+        mSpinner.setAdapter(madapter);
 
+        //이벤트
         mListView.setOnItemClickListener(this);
+        mListView.setOnItemLongClickListener(this);
+        mGridView.setOnItemClickListener(this);
+        mGridView.setOnItemLongClickListener(this);
     }
 
 
@@ -60,5 +75,15 @@ public class WeatherActivity extends AppCompatActivity implements AdapterView.On
         madapter.setSelect(position);
         //데이터가 변경됨을 알려줌 = 다시그려라
         madapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        //롱 클릭시 해당 아이템 삭제
+        mWeatherList.remove(position);
+
+        //어댑터에 변경을 통지 = 다시 그려라
+        madapter.notifyDataSetChanged();
+        return false;
     }
 }
